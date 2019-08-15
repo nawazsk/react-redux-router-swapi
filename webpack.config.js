24ab6f5321d9f42128bd35f62adccde8dcf7f3bb
nawazsk <nawaz.540@gmail.com>
2019-08-15 12:17:36 +0530

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'bundle.js',
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test:/\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test:/\.html$/,
+                use: {
+                    loader: "html-loader"
+                }
+            }
+        ]
+    },
+    devServer: {
+        historyApiFallback: true,
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        })
+    ]
+};
